ced44953d22dc3a9706044e213b7d4248b9f7054
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-31 22:05:36 +0530

    adding few more patterns

---
+++ Dsa C++ (tuf)/PATTERNS/Left_pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void left_pyramid(int n) {
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < ; j++)
+        {
+            cout << 
+        }
+        
+    }
+    
+}
+int main() {
+    int n;
+    cout << "Enter the number lines you want ";
+    cin >> n;
+    
+    left_pyramid(n);
+
+    return 0;
+}
